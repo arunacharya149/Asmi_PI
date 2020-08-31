6b32343b9a1d212591d4b0522704b674a4a7369f
Arun <arunacharya149@gmail.com>
2020-08-31 16:48:13 +0545

    python script added

--- Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
+++ Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
@@ -2,12 +2,23 @@ package com.example.asmi_pi;
 
 import android.util.Log;
 
-public class AsmiInjection {
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+
+public class AsmiInjection extends Python.Platform {
 
     private static final String TAG = "%%%%i am in Library---";
 
-    public static void d(String message) {
-        Log.d(TAG, message);
+    public void d(String message) {
+
+        if (! Python.isStarted()) {
+            Python.start(this);
+        }
+
+        Python py = Python.getInstance();
+        PyObject pyObj = py.getModule("script");
+        PyObject obj = pyObj.callAttr("showMessage",message.toString());
+        Log.d(TAG, obj.toString());
     }
 
 }
