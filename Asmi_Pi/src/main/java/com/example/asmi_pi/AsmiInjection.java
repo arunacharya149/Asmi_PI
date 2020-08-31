cd3bbacd9bfcf382f3f405e0129f0539bc7b5721
Arun <arunacharya149@gmail.com>
2020-08-31 19:37:15 +0545

    last try

--- Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
+++ Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
@@ -9,15 +9,18 @@ import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
-public class AsmiInjection extends AppCompatActivity {
+public class AsmiInjection {
+
+
 
     private static final String TAG = "%%%%i am in Library---";
 
     public void d(String message) {
 
         if (! Python.isStarted()) {
-            Python.start(new AndroidPlatform(this));
+            Python.start(Python.getPlatform());
         }
+
 
         Python py = Python.getInstance();
         PyObject pyObj = py.getModule("script");
