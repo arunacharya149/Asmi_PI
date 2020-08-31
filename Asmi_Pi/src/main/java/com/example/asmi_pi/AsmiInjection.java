4559a08a36057fd2eced8c2958fb86bf119fc3a2
Arun <arunacharya149@gmail.com>
2020-08-31 18:48:43 +0545

    add appCompatibility

--- Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
+++ Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
@@ -1,15 +1,23 @@
 package com.example.asmi_pi;
 
+import android.content.Context;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
-public class AsmiInjection {
+public class AsmiInjection extends AppCompatActivity {
 
     private static final String TAG = "%%%%i am in Library---";
 
     public void d(String message) {
+
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
 
         Python py = Python.getInstance();
         PyObject pyObj = py.getModule("script");
--- app/src/main/java/com/example/asmi_pi_example/MainActivity.java
+++ app/src/main/java/com/example/asmi_pi_example/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (! Python.isStarted()) {
-            Python.start(new AndroidPlatform(this));
-        }
+
     }
 }
