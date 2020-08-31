f345e187b21e19f790408b6d13c6535fedb8bb29
Arun <arunacharya149@gmail.com>
2020-08-31 15:33:54 +0545

    first commit

---
+++ Asmi_Pi/src/main/java/com/example/asmi_pi/AsmiInjection.java
@@ -0,0 +1,13 @@
+package com.example.asmi_pi;
+
+import android.util.Log;
+
+public class AsmiInjection {
+
+    private static final String TAG = "%%%%i am in Library---";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+
+}
